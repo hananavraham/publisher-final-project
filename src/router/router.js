7c2e6d2db74929c9fedb73ba1f57063d8a374c65
hananavr <evamiller1307>
2018-07-05 12:00:47 +0300

    first commit

---
+++ src/router/router.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Route } from "react-router-dom";
+import Login from "../Components/login";
+
+const ReactRouter =()=>{
+    return (
+        <React.Fragment>
+            <Login />
+        </React.Fragment>
+    );}
+
+export default ReactRouter;
